3bb0518a8155bcb67509a9863c168168e11828e1
thor <otaviosilva2632@gmail.com>
2021-04-18 20:44:18 -0300

    feat: configure brave browser to install

--- init.sh
+++ init.sh
@@ -51,6 +51,14 @@ sudo npm install -g yarn -y
 sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt install snapd
 
+## install brave browser ##
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+
 ## finalization, update and cleaning ##
 sudo apt update && sudo apt dist-upgrade -y
 sudo apt autoclean
